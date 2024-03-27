814b2d3e0e2aa360ea7535ac89d275cb35bd1539
jschouw <jeff.schouw@gmail.com>
2024-03-26 20:19:18 -0400

    Merge button logic, without the actual merge action

--- src/utils.py
+++ src/utils.py
@@ -4,15 +4,8 @@
     Functionality and logic of the program, including both GUI creation and PDF manipulation.
 """
 
-# the intended audience for this project is an everyday office employee needing to manipulate
-# PDF files in simple ways without having to pay for Adobe Acrobat. complement Acrobat free
-# version, offering functionality that it does not, or that you normally would need to pay for.
-# it will require a simple GUI for the layperson, but should also allow CLI use for the tech-inclined.
-# this program is essentially an easier-to-use wrapper for pypdf
 
-
-# [brainstorm]
-# acrobat viewer is the free version, and it can basically only open PDF files.
+# [brainstorming:]
 # functionality brainstorming: merge, splice, split, metadata operations, extract text, extract images,
 # encrypt/password, reduce file size
 # Edit text and images, reorder, and delete pages in a PDF
@@ -20,36 +13,17 @@
 # Redact to permanently remove sensitive visible information
 # Compare two versions of a PDF to review all differences
 
-# GUI brainstorm:
-# 1 main window with a logo and a series of buttons, each button opening the respective PDF process
-# merge button will open a window that allows you to select multiple files to merge, then opens a 2nd dialog for confirmation of the order of documents
-# split button will open a window that allows you to select just 1 file, and then a 2nd dialog for confirmation of the split location
-# splice button will open a window that shows the PDF, and alongside it will be a text entry box for the page(s), with a 2nd dialog for confirmation that shows the new PDF
-# metadata operations button will open a filechooser, then ask what operation to do, then confirm
-# extracta text and extract images buttons will
-
-# CLI brainstorm:
-#
-
-# program structure brainstorm:
-#
-
 # ****************************************************************************
 
 import os
-import tkinter
 from datetime import datetime
 from pypdf import PdfReader
 from pypdf import PdfWriter
 from tkinter import messagebox
+from tkinter import filedialog
 
 
 def split_pdfs():
-    """Splits PDFs in working directory.
-
-    Splits all PDF files in half by page number and outputs two long PDF files
-    of the merged halves.
-    """
     pdf_writer_front = PdfWriter()
     pdf_writer_back = PdfWriter()
 
@@ -67,7 +41,7 @@ def split_pdfs():
                 pdf_writer_back.append(fileobj=pdf_reader, pages=(int(pagecount / 2), pagecount))
 
     if count == 0:
-        return tkinter.messagebox.showerror(title="Error: No files", message="Error: No PDF files to split.")
+        return messagebox.showerror(title="Error: No files", message="Error: No PDF files to split.")
 
     current_time = str(datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
     front_output = open(current_time + "-front-split-output.pdf", "wb")
@@ -76,12 +50,30 @@ def split_pdfs():
     pdf_writer_back.write(back_output)
     pdf_writer_front.close()
     pdf_writer_back.close()
-    tkinter.messagebox.showinfo("PDF Split Completed", f"{count} file(s) split.")
+    messagebox.showinfo("PDF Split Completed", f"{count} file(s) split.")
 
 
 def merge_pdfs():
-    """Merges PDFs in working directory."""
+    """Allows the user to select multiple files to merge, as well as what order to merge them in."""
 
+    valid = False
+    while not valid:  # 'not' checks for false boolean value
+        file = filedialog.askopenfilenames(
+            filetypes=[("PDF files", "*.pdf")],
+            title="Please select files to merge:",
+            initialdir=os.getcwd())
+
+        if not file:
+            return 1  # If file returns false the cancel button was pressed, so nothing and return to main menu
+
+        if len(file) <= 1:  # More than 1 file must be chosen to merge
+            messagebox.showerror(title="Error: Not enough files chosen",
+                                 message="Error: You must choose more than 1 file to merge.")
+        else:
+            print('merge function here')
+
+
+"""
     pdf_writer = PdfWriter()
     count = 0  # Count for total files merged
     for filename in os.listdir('..'):
@@ -100,6 +92,6 @@ def merge_pdfs():
     pdf_writer.write(output)
     pdf_writer.close()
     tkinter.messagebox.showinfo("PDF Merge Completed", f"{count} files merged.")
-
+"""
 
 
